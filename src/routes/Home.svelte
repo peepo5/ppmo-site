bb131e94bb2ea17f95522a96bb8bd0f6947dd71e
NomNomBiscuit <72892531+peepopoggers@users.noreply.github.com>
2022-08-07 22:03:06 +0100

    Updated physical print grammar

--- src/routes/Home.svelte
+++ src/routes/Home.svelte
@@ -10,6 +10,6 @@
 	<p style="margin-top: 3px;">Freedom comes from understanding.<br> A book to become free from the porn and masturbation trap.</p>
 
 	<Link to="/chapter/0"><button class="button">Read the book</button></Link><br><br>
-	<a href="https://www.lulu.com/shop/cat-guy/ppmo-paperback/paperback/product-gmz9p6.html" target="_blank" title="Print on demand. No profit made on this.">Physical copy</a> |
+	<a href="https://www.lulu.com/shop/cat-guy/ppmo-paperback/paperback/product-gmz9p6.html" target="_blank" title="Print on demand. No profit made on this. Use code THANKYOU20.">Physical Copy</a> |
 	<a href="https://opencollective.com/ppmo-collective-fund" target="_blank" title="Donate to help out the project">Donate</a>
 </main>
